56a3429fb7923d0c82eb4ee6ff85357c8d607c1a
vleonecezar <vleone.social@gmail.com>
2022-03-01 21:33:40 -0300

    teste com github-pages

--- src/components/characters/index.js
+++ src/components/characters/index.js
@@ -1,43 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import './styles.css'
+import React, { useState, useEffect } from "react";
+import "./styles.css";
 
 async function getCharacters() {
-    try {
-        let response = await fetch('http://localhost:3000/api/characters.json')
-        let data = await response.json()
-        return data
-    } catch (error) {
-        console.error(error)
-    }
+  try {
+    let response = await fetch(
+      "https://vleonecezar.github.io/dragonball-fansite/api/characters.json"
+    );
+    let data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
 }
 
 const Characters = () => {
+  const [characters, setCharacters] = useState([]);
 
-    const [characters, setCharacters] = useState([])
-
-    useEffect(() => {
-        getCharacters().then(data => {
-            setCharacters(
-                data['characters'].map(({ id, image, name }) => {
-                    return(
-                        <div key={id} className='character'>
-                            <img src={image} alt={`foto de ${name}`}/>
-                            <p>{name}</p>
-                        </div>
-                    )
-                })
-            )
-        })
-    }, [])
-
-    return (
-        <div className='main-characters'>
-            <h2>Principais Personagens</h2>
-            <div className='characters'>
-                {characters}
+  useEffect(() => {
+    getCharacters().then((data) => {
+      setCharacters(
+        data["characters"].map(({ id, image, name }) => {
+          return (
+            <div key={id} className="character">
+              <img src={image} alt={`foto de ${name}`} />
+              <p>{name} teste</p>
             </div>
-        </div>
-    )
-}
+          );
+        })
+      );
+    });
+  }, []);
 
-export default Characters
+  return (
+    <div className="main-characters">
+      <h2>Principais Personagens</h2>
+      <div className="characters">{characters}</div>
+    </div>
+  );
+};
+
+export default Characters;
--- src/routes.js
+++ src/routes.js
@@ -1,20 +1,22 @@
-import React from 'react'
-import { Switch, Route } from 'react-router-dom'
-import AboutScreen from './screens/about'
-import CharactersScreen from './screens/characters'
-import AuthorScreen from './screens/author'
-import PageNotFoundScreen from './screens/pagenotfound'
+import React from "react";
+import { Switch, Route } from "react-router-dom";
+import AboutScreen from "./screens/about";
+import CharactersScreen from "./screens/characters";
+import AuthorScreen from "./screens/author";
+import PageNotFoundScreen from "./screens/pagenotfound";
 
 const Routes = () => {
-    return (
-        <Switch>
-            <Route exact path='/' component={AboutScreen}/>
-            <Route exact path='/sobre' component={AboutScreen}/>
-            <Route exact path='/personagens' component={CharactersScreen}/>
-            <Route exact path='/autor' component={AuthorScreen}/>
-            <Route path='*' component={PageNotFoundScreen}/>
-        </Switch>
-    )
-}
+  return (
+    <Switch>
+      <Route exact path="/" component={AboutScreen} />
+      <Route exact path="/sobre" component={AboutScreen} />
+      <Route exact path="/personagens" component={CharactersScreen} />
+      <Route exact path="/autor" component={AuthorScreen} />
+      <Route exact path="/dragonball-fansite" component={AboutScreen} />{" "}
+      {/* "/dragonball-fansite" é para o github-pages, evitando erro 404 */}
+      <Route path="*" component={PageNotFoundScreen} />
+    </Switch>
+  );
+};
 
-export default Routes
+export default Routes;
